9a261e015ee866a7ed8af56374836797da0a9b4a
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-01-03 17:11:15 -0600

    Add Services

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,5 @@
+class ServicesController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 SynergySolutions::Application.routes.draw do
+  get "services", :to => "services#index"
   get "about", :to => "about#index"
-
   get "administration", :to => "administration#index"
-
   devise_for :admins do
     get "login", :to => "devise/sessions#new"
     get "logout", :to => "devise/sessions#destroy"
   end
   resources :admins
-  
   get "home", :to => "home#index"
-  
   root :to => "home#index"
 end
