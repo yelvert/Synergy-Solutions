8e70e3866b233bf2f74b8e82066c2f7afd7cdc05
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-01-03 10:12:10 -0600

    Init Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SynergySolutions::Application.config.secret_token = '127d902ad6be038e8cdd21b6259011dc3ee021af310c071199a3254e7fd6e93ad081ab5a47022c0f1350180c944678f6d8a64947b6c132f68c34ea7c073b72e6'
